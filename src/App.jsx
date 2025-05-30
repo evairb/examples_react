7c82461f247541d79e47d8c82bccada6061232d9
Evair Domingues <evairbd@gmail.com>
2025-05-30 08:37:05 -0300

    Example useRef in timeOut

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,21 @@
 import React, { useRef, useState } from 'react';
 
 const App = () => {
-  const [comentarios, setComentarios] = useState([]);
-  const [input, setInput] = useState('');
-  const inputElement = useRef();
-
+  const [carrinho, setCarrinho] = useState(0);
+  const [notificacao, setNotificacao] = useState(null);
+  const timeoutRef = useRef;
   function handleClick() {
-    setComentarios([...comentarios, input]);
-    setInput('');
-    inputElement.currentfocus();
+    setCarrinho(carrinho + 1);
+    setNotificacao('Item adicionado ao carrinho');
+    setTimeout(() => {
+      setNotificacao(null);
+    }, 2000);
   }
 
   return (
     <div>
-      <ul>
-        {comentarios.map(comentario => (
-          <li key={comentario}>{comentario}</li>
-        ))}
-      </ul>
-      <input
-        type='text'
-        ref={inputElement}
-        value={input}
-        onChange={({ target }) => setInput(target.value)}
-      />
-      <br />
-      <button onClick={handleClick}>Enviar</button>
+      <p>{notificacao}</p>
+      <button onClick={handleClick}>Adicionar Carrinho {carrinho}</button>
     </div>
   );
 };
