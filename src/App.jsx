090d57fa5f559d7345e99777559941f7d0b8ac97
Evair Domingues <evairbd@gmail.com>
2025-06-03 13:50:59 -0300

    example forms 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,94 @@
 import React from 'react';
 
 const App = () => {
-  const [form, setForm] = React.useState({
-    nome: '',
-    email: '',
-  });
+  const formFields = [
+    {
+      id: 'nome',
+      label: 'Nome',
+      type: 'text',
+    },
+    {
+      id: 'email',
+      label: 'Email',
+      type: 'email',
+    },
+    {
+      id: 'senha',
+      label: 'Senha',
+      type: 'password',
+    },
+    {
+      id: 'cep',
+      label: 'Cep',
+      type: 'text',
+    },
+    {
+      id: 'rua',
+      label: 'Rua',
+      type: 'text',
+    },
+    {
+      id: 'numero',
+      label: 'Numero',
+      type: 'text',
+    },
+    {
+      id: 'bairro',
+      label: 'Bairro',
+      type: 'text',
+    },
+    {
+      id: 'cidade',
+      label: 'Cidade',
+      type: 'text',
+    },
+    {
+      id: 'estado',
+      label: 'Estado',
+      type: 'text',
+    },
+  ];
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log(event);
-  }
+  const [form, setForm] = React.useState(
+    formFields.reduce((acc, field) => {
+      return {
+        ...acc,
+        [field.id]: '',
+      };
+    }, {}),
+  );
+  const [response, setResponse] = React.useState(null);
 
   function handleChange({ target }) {
     const { id, value } = target;
     setForm({ ...form, [id]: value });
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    fetch('https://ranekapi.origamid.dev/json/api/usuario', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(form),
+    }).then(response => {
+      setResponse(response);
+    });
+  }
+
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor='nome'>Nome</label>
-      <input
-        id='nome'
-        type='text'
-        name='nome'
-        value={form.nome}
-        onChange={handleChange}
-      />
-
-      <label htmlFor='email'>Email</label>
-      <input
-        id='email'
-        type='text'
-        name='email'
-        value={form.email}
-        onChange={handleChange}
-      />
+      {formFields.map(({ id, type, label }) => (
+        <div key={id}>
+          <label htmlFor={id}>{label}</label>
+          <input type={type} id={id} value={form[id]} onChange={handleChange} />
+        </div>
+      ))}
+      {response && response.ok && <p>Formulario Enviado</p>}
       <button>Enviar</button>
     </form>
   );
 };
-
 export default App;
 // Os links abaixo puxam dados de um produto em formato JSON
 // https://ranekapi.origamid.dev/json/api/produto/tablet
