1575d65067f65512a62eb625278c27d7fd8b5a4b
Evair Domingues <evairbd@gmail.com>
2025-06-03 14:31:51 -0300

    example checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,38 @@
 import React from 'react';
 
 const App = () => {
-  const formFields = [
-    {
-      id: 'nome',
-      label: 'Nome',
-      type: 'text',
-    },
-    {
-      id: 'email',
-      label: 'Email',
-      type: 'email',
-    },
-    {
-      id: 'senha',
-      label: 'Senha',
-      type: 'password',
-    },
-    {
-      id: 'cep',
-      label: 'Cep',
-      type: 'text',
-    },
-    {
-      id: 'rua',
-      label: 'Rua',
-      type: 'text',
-    },
-    {
-      id: 'numero',
-      label: 'Numero',
-      type: 'text',
-    },
-    {
-      id: 'bairro',
-      label: 'Bairro',
-      type: 'text',
-    },
-    {
-      id: 'cidade',
-      label: 'Cidade',
-      type: 'text',
-    },
-    {
-      id: 'estado',
-      label: 'Estado',
-      type: 'text',
-    },
-  ];
-
-  const [form, setForm] = React.useState(
-    formFields.reduce((acc, field) => {
-      return {
-        ...acc,
-        [field.id]: '',
-      };
-    }, {}),
-  );
-  const [response, setResponse] = React.useState(null);
+  const [cores, setCores] = React.useState(['vermelho']);
 
   function handleChange({ target }) {
-    const { id, value } = target;
-    setForm({ ...form, [id]: value });
+    if (target.checked) {
+      setCores([...cores, target.value]);
+    } else {
+      setCores(cores.filter(cor => cor !== target.value));
+    }
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    fetch('https://ranekapi.origamid.dev/json/api/usuario', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(form),
-    }).then(response => {
-      setResponse(response);
-    });
-  }
+  console.log(cores);
 
   return (
-    <form onSubmit={handleSubmit}>
-      {formFields.map(({ id, type, label }) => (
-        <div key={id}>
-          <label htmlFor={id}>{label}</label>
-          <input type={type} id={id} value={form[id]} onChange={handleChange} />
-        </div>
-      ))}
-      {response && response.ok && <p>Formulario Enviado</p>}
-      <button>Enviar</button>
+    <form>
+      <label>
+        <input
+          type='checkbox'
+          checked={cores.includes('azul')}
+          value='azul'
+          onChange={handleChange}
+        />
+        Azul
+      </label>
+      <label>
+        <input
+          type='checkbox'
+          value='vermelho'
+          checked={cores.includes('vermelho')}
+          onChange={handleChange}
+        />
+        Vermelho
+      </label>
     </form>
   );
 };
