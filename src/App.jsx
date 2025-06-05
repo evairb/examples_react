df2acc68ccf4d5018b6944102a35ce5e8a283e7f
Evair Domingues <evairbd@gmail.com>
2025-06-05 14:12:10 -0300

    Example Checkbox and radio

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import Input from './Form/Input';
 import Select from './Form/Select';
+import Radio from './Form/Radio';
+import Checkbox from './Form/Checkbox';
 
 const App = () => {
   const [nome, setNome] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [produto, setProduto] = React.useState('');
+  const [cor, setCor] = React.useState('vermelho');
+  const [linguagem, setLinguagem] = React.useState('');
 
   return (
     <form>
+      <h2>Checkbox</h2>
+      <Checkbox
+        options={['JavaScript', 'PHP', 'Python']}
+        value={linguagem}
+        setValue={setLinguagem}
+      />
+      <Radio options={['azul', 'vermelho']} value={cor} setValue={setCor} />
       <Select
         options={['smartphone', 'tablet']}
         value={produto}
