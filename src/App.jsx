bdf6513f16679fb8f00b67a9b7646dbec3a66197
Evair Domingues <evairbd@gmail.com>
2025-06-06 14:15:11 -0300

    example module css

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,26 @@
 import React from 'react';
-import Radio from './Form/Radio';
-
-const perguntas = [
-  {
-    pergunta: 'Qual método é utilizado para criar componentes?',
-    options: [
-      'React.makeComponent()',
-      'React.createComponent()',
-      'React.createElement()',
-    ],
-    resposta: 'React.createElement()',
-    id: 'p1',
-  },
-  {
-    pergunta: 'Como importamos um componente externo?',
-    options: [
-      'import Component from "./Component"',
-      'require("./Component")',
-      'import "./Component"',
-    ],
-    resposta: 'import Component from "./Component"',
-    id: 'p2',
-  },
-  {
-    pergunta: 'Qual hook não é nativo?',
-    options: ['useEffect()', 'useFetch()', 'useCallback()'],
-    resposta: 'useFetch()',
-    id: 'p3',
-  },
-  {
-    pergunta: 'Qual palavra deve ser utilizada para criarmos um hook?',
-    options: ['set', 'get', 'use'],
-    resposta: 'use',
-    id: 'p4',
-  },
-];
+import Slide from './Slide';
 
 const App = () => {
-  const [respostas, setRespostas] = React.useState({
-    p1: '',
-    p2: '',
-    p3: '',
-    p4: '',
-  });
-
-  const [slide, setSlide] = React.useState(0);
-  const [resultado, setResultado] = React.useState(null);
-
-  function resultadoFinal() {
-    console.log('Final');
-    const corretas = perguntas.filter(
-      ({ id, resposta }) => respostas[id] === resposta,
-    );
-    setResultado(`Voce acertou: ${corretas.length} de ${perguntas.length}`);
-    console.log(corretas);
-  }
-
-  function handleClick() {
-    if (slide < perguntas.length - 1) {
-      setSlide(slide + 1);
-    } else {
-      setSlide(slide + 1);
-      resultadoFinal();
-    }
-  }
-
-  function handleChange({ target }) {
-    setRespostas({ ...respostas, [target.id]: target.value });
-  }
+  const slides = [
+    {
+      id: 'slide1',
+      text: 'Slide 1',
+    },
+    {
+      id: 'slide2',
+      text: 'Slide 2',
+    },
+    {
+      id: 'slide3',
+      text: 'Slide 3',
+    },
+  ];
 
   return (
-    <form onSubmit={event => event.preventDefault()}>
-      {perguntas.map((pergunta, index) => (
-        <Radio
-          active={slide === index}
-          key={pergunta.id}
-          value={respostas[pergunta.id]}
-          onChange={handleChange}
-          {...pergunta}
-        />
-      ))}
-      {resultado ? (
-        <p>{resultado}</p>
-      ) : (
-        <button onClick={handleClick}>Proxima</button>
-      )}
-    </form>
+    <div>
+      <Slide slides={slides} />
+    </div>
   );
 };
 export default App;
